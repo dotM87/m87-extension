936efc71863d10ddcd1a53f2396e4ff2ab68507a
Miguel Benjamin Zubieta Rios <miguel.zubieta@proton.me>
2025-04-16 16:03:18 -0400

    Refactor Popup.vue to use Tailwind CSS for styling and remove unused popup.css file

--- src/pages/Popup.vue
+++ src/pages/Popup.vue
@@ -3,59 +3,11 @@ console.log("Hello from the popup!");
 </script>
 
 <template>
-  <div>
-    <img src="/icon-with-shadow.svg" />
-    <h1>vite-plugin-web-extension</h1>
-    <p>
-      Template: <code>vue-ts</code>
+  <div class="w-[300px] h-[400px] m-0 p-0 bg-[rgb(36,36,36)] flex flex-col gap-4 items-center justify-center">
+    <img class="w-[200px] h-[200px]" src="/icon-with-shadow.svg" />
+    <h1 class="text-[18px] text-white font-bold m-0">vite-plugin-web-extension</h1>
+    <p class="text-white opacity-70 m-0">
+      Template: <code class="text-[12px] px-1 py-0.5 bg-[#ffffff24] rounded">vue-ts</code>
     </p>
   </div>
 </template>
-
-<style>
-html,
-body {
-  width: 300px;
-  height: 400px;
-  padding: 0;
-  margin: 0;
-}
-
-body {
-  background-color: rgb(36, 36, 36);
-}
-
-body > div {
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-  align-items: center;
-  justify-content: center;
-}
-
-img {
-  width: 200px;
-  height: 200px;
-}
-
-h1 {
-  font-size: 18px;
-  color: white;
-  font-weight: bold;
-  margin: 0;
-}
-
-p {
-  color: white;
-  opacity: 0.7;
-  margin: 0;
-}
-
-code {
-  font-size: 12px;
-  padding: 2px 4px;
-  background-color: #ffffff24;
-  border-radius: 2px;
-}
-</style>
